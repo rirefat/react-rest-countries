e69e3a4ec1f50c98ea9da16caefe8dd214570068
rirefat <rirefat.official@gmail.com>
2022-03-18 00:46:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
     </div>
   );
+}
+
+function Countries(){
+  return(
+    <div>
+      <h2>Visiting React Rest Countries</h2>
+    </div>
+  )
 }
 
 export default App;
