71c083c10ff97e03de2f1681fbf82a4f2e2c2281
rirefat <rirefat.official@gmail.com>
2022-03-19 16:25:39 +0600

    Added Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
+import Country from './components/Countries/Countries';
 
 function App() {  
   return (
     <div className="App">
-      <Countries></Countries>
+      <Header totalCountry={'country.length'}></Header>
+      <Country></Country>
     </div>
   );
 }
 
-function Countries(){
-  const [country, setCountry]=useState([]);
+const LoadCountries= ()=>{
+  const [country, SetCountry]=useState([]);
   useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
-      .then(res=>res.json())
-      .then(data=>setCountry(data))
+      .then(response => response.json())
+      .then(data => SetCountry(data))
   },[])
   return(
     <div>
-      <h1>Visiting All the Countries</h1>
-    <p>Available Countries: {country.length}</p> 
+      <h3>Available Country: {country.length}</h3>
     </div>
   )
 }
+
+// function LoadCountries(){
+//   const [countries, setCountry]=useState([]);
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res=>res.json())
+//       .then(data=>setCountry(data))
+//   },[])
+//   return(
+//     <div>
+//       <h1>Visiting All the Countries</h1>
+//       <p><strong>Available Countries: </strong>{countries.length}</p> 
+//       {
+//         countries.map(country => <Country name={country.name.common}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// function Country(props){
+//   return(
+//     <div>
+//       <p><strong>Name of Country: </strong>{props.name}</p>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>Basic Information of All The Countries.</h1>
+        </div>
+    );
+};
+
+export default Header;
